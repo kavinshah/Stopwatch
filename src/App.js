09e69238061549e5f3c85013ef59558f602e9d60
kavinshah <38910831+kavinshah@users.noreply.github.com>
2024-02-10 21:29:16 +0530

    reorganizing states for timer and clock components

--- src/App.js
+++ src/App.js
@@ -1,133 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-class Clock extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      sessionLength:this.props.sessionLength,
-      breakLength:this.props.breakLength,
-      minute:25,
-      second:0,
-	  sessionTimer:true
-    };
-    this.handleSessionIncreament=this.handleSessionIncreament.bind(this);
-    this.handleSessionDecreament=this.handleSessionDecreament.bind(this);
-    this.handleBreakIncreament=this.handleBreakIncreament.bind(this);
-    this.handleBreakDecreament=this.handleBreakDecreament.bind(this);
-    this.handleReset=this.handleReset.bind(this);
-    this.handleStartStop=this.handleStartStop.bind(this);
-    this.updateClock=this.updateClock.bind(this);
-    this.playBeep = this.playBeep.bind(this);
-	this.clockInterval=null;
-  }
-  
-  handleSessionIncreament(event){
-    this.setState((state)=>({
-      sessionLength:Math.min(60, state.sessionLength+1),
-      minute:Math.min(60, state.sessionLength+1)
-    }));
-  }
-  
-  handleSessionDecreament(event){
-    this.setState((state)=>({
-      sessionLength:Math.max(1, state.sessionLength-1),
-      minute:Math.max(1, state.minute-1)
-    }));
-  }
-  
-  handleBreakIncreament(event){
-    this.setState((state)=>({
-      breakLength:Math.min(60,state.breakLength+1)
-    }));
-    
-  }
-  
-  handleBreakDecreament(event){
-    this.setState((state)=>({
-      breakLength:Math.max(1,state.breakLength-1)
-    }));
-  }
-  
-  handleReset(event){
-	clearInterval(this.clockInterval);
-	this.clockInterval=null;
-	this.rewindBeep();
-	this.setState((state)=>({
-	  minute:25,
-	  second:0,
-	  sessionLength:25,
-	  breakLength:5,
-	  sessionTimer:true
-	}));
-  }
-  
-  handleStartStop(event){
-	if(this.clockInterval !== null){
-		//console.log('paused');
-		clearInterval(this.clockInterval);		
-		this.clockInterval=null;
-	} else{
-		//console.log('started');
-		this.clockInterval = setInterval(this.updateClock, 1000);
+function Clock({bl, sl}){
+	const [sessionTimer, setSessionTimer] = useState(true);
+	const [sessionLength, setSessionLength] = useState(sl);
+	const [breakLength, setBreakLength] = useState(bl);
+	
+	const handleSessionIncreament = (event) => {
+		setSessionLength(Math.min(60, sessionLength+1));
 	}
-  }
-  
-  updateClock(){
-    //console.log(`minute: ${this.state.minute}, seconds:${this.state.second}`);
-    if(this.state.minute===0 && this.state.second === 0){
-	  this.playBeep();
-	  if(this.state.sessionTimer){
-		//console.log('break timer started');
-		this.setState((state)=>({
-							  minute:state.breakLength,
-							  second:0,
-							  sessionTimer:false
-						  }));
-	  } else{
-		//console.log('session timer started');
-		this.setState((state)=>({
-							  minute:state.sessionLength,
-							  second:0,
-							  sessionTimer:true
-						  }));
-	  }
-	  
-    }
-    else if(this.state.second <= 0){
-      //reduce minutes and reset second to 59
-	  this.setState((state)=>({
-							  minute:state.minute-1,
-							  second:59
-						  }));
-    } else{
-      this.setState((state)=>({
-							  minute:state.minute,
-							  second:state.second-1
-						  }));
-    }
-  }
-  
-  playBeep(){
-    let element = document.getElementById('beep');
-    element.play();
-  }
-  
-  rewindBeep(){
-    let element = document.getElementById('beep');
-    element.load();
-  }
-  
-  render(){
-    return (
-    <div id='Clock' className='clock'>
-      <ToggleLength name={'Break Length'} currentValue={this.state.breakLength} toggleId={'break-length'} labelId={'break-label'} upId={'break-increment'} downId={'break-decrement'} handleIncreament={this.handleBreakIncreament} handleDecreament={this.handleBreakDecreament} />
-      <ToggleLength name={'Session Length'} currentValue={this.state.sessionLength} toggleId={'session-length'} labelId={'session-label'} upId={'session-increment'} downId={'session-decrement'} handleIncreament={this.handleSessionIncreament} handleDecreament={this.handleSessionDecreament} />
-      <Timer timerType={this.state.sessionTimer?'Session':'Break'} minute={String(this.state.minute)} second={String(this.state.second)} handleReset={this.handleReset} handleStartStop={this.handleStartStop} />
-	  <audio id="beep" preload="auto" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/BeepSound.wav"></audio>
-    </div>
-    );
-  }
+
+	const handleSessionDecreament = (event) => {
+		setSessionLength(Math.max(1, sessionLength-1));
+	}
+	
+	const handleBreakIncreament = (event) => {
+		setBreakLength(Math.min(60, breakLength+1));
+	}
+	
+	const handleBreakDecreament = (event) => {
+		setBreakLength(Math.max(1, breakLength-1));
+	}
+	
+	const handleResetButtonClick = (event) => {
+		setSessionLength(25);
+		setBreakLength(5);
+	}
+	
+	return (
+		<div id='Clock' className='clock'>
+		  <ToggleLength name={'Break Length'} currentValue={breakLength} toggleId={'break-length'} labelId={'break-label'} upId={'break-increment'} downId={'break-decrement'} handleIncreament={handleBreakIncreament} handleDecreament={handleBreakDecreament} />
+		  <ToggleLength name={'Session Length'} currentValue={sessionLength} toggleId={'session-length'} labelId={'session-label'} upId={'session-increment'} downId={'session-decrement'} handleIncreament={handleSessionIncreament} handleDecreament={handleSessionDecreament} />
+		  <Timer sessionLength={sessionLength} breakLength={breakLength} handleResetButtonClick={handleResetButtonClick} />
+		  <audio id="beep" preload="auto" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/BeepSound.wav"></audio>
+		</div>
+	);
 }
 
 function ToggleLength({labelId, name, toggleId, currentValue, downId, upId, handleDecreament, handleIncreament}){
@@ -143,11 +50,86 @@ function ToggleLength({labelId, name, toggleId, currentValue, downId, upId, hand
     );
 }
 
-function Timer({timerType, minute, second, handleStartStop, handleReset}){
+function Timer({sessionLength, breakLength, handleResetButtonClick}){
+	const [sessionTimer, setSessionTimer] = useState(true);
+	const [minutes, setMinutes] = useState(sessionLength);
+	const [seconds, setSeconds] = useState(0);
+	const [timerStarted, setTimerStarted] = useState(false);
+	const [changeAllowed, setChangeAllowed] = useState(true);
+	
+	const handleStartStop = (event) => {
+		if(!timerStarted){
+			setTimerStarted(true);
+		} else{
+			setTimerStarted(false);
+		}
+	}
+	
+	const handleReset = (event) => {
+		setTimerStarted(false);
+		handleResetButtonClick(event);
+		setChangeAllowed(true);
+		setMinutes(25);
+		setSeconds(0);
+	}
+	
+	useEffect(()=>{
+		let intervalId;
+		if(timerStarted){
+			intervalId= setInterval(()=>{
+				updateClock();
+			}, 1000);
+		}
+		else if(changeAllowed){
+			setMinutes(sessionLength);
+			setSeconds(0);
+		}
+		return () => {
+			clearInterval(intervalId);
+		}
+	});
+	
+	const updateClock = () => {
+		//console.log(`Entering: minutes: ${minutes}, seconds:${seconds}, timer:${clockInterval}`);
+		if(minutes === 0 && seconds === 0){
+			playBeep();
+			if(sessionTimer){
+				//console.log('break timer started');
+				setMinutes(breakLength);
+				setSeconds(0);
+				setSessionTimer(false);
+			} else{
+				//console.log('session timer started');
+				setMinutes(sessionLength);
+				setSeconds(0);
+				setSessionTimer(true);
+			}
+		}
+		else if(seconds === 0){
+			//reduce minutes and reset second to 59
+			setMinutes((prev) => prev-1);
+			setSeconds(59);
+		} else if(minutes >= 0 && seconds > 0){
+			setMinutes((prev) => prev);
+			setSeconds((prev) => prev-1);
+		}
+		//console.log(`Leaving: minute: ${minutes}, seconds:${seconds}, timer:${clockInterval}`);
+	}
+	
+	const playBeep = () => {
+		let element = document.getElementById('beep');
+		element.play();
+	}
+	
+	const rewindBeep = () => {
+		let element = document.getElementById('beep');
+		element.load();
+	}
+	
 	return (
       <div>
-        <label id='timer-label'>{timerType + ' : '}</label>
-        <label id='time-left'>{String(minute).padStart(2,'0')}:{second.padStart(2,'0')}</label>
+        <label id='timer-label'>{sessionTimer?'Session : ':'Break : '}</label>
+        <label id='time-left'>{String(minutes).padStart(2,'0')}:{String(seconds).padStart(2,'0')}</label>
         <div>
           <button id='start_stop' onClick={handleStartStop}>Start/Stop</button>
           <button id='reset' onClick={handleReset}>Reset</button>
@@ -157,8 +139,8 @@ function Timer({timerType, minute, second, handleStartStop, handleReset}){
 }
 
 Clock.defaultProps = {
-  sessionLength:25,
-  breakLength:5
+  sl:25,
+  bl:5
 };
 
 export default Clock;
